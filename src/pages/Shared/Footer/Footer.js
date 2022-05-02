a9e6c2914c9c9160521a50f2e95ebb53faca4bd5
sumon092 <m.sumon@outlook.com>
2022-05-03 02:24:46 +0600

    project initialize

---
+++ src/pages/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p><small>All Rights Reserved Royal Auto &copy; 2022</small></p>
+        </div>
+    );
+};
+
+export default Footer;
