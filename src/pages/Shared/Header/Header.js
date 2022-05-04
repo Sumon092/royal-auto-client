058b2a3a1589b386fc7556774b699112fdbd65a5
sumon092 <m.sumon@outlook.com>
2022-05-05 01:41:35 +0600

    nav bar added

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,10 +1,30 @@
 import React from 'react';
-
+import { Container, Navbar } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav'
+import logo from '../../../images/logo/logo.svg'
 const Header = () => {
     return (
-        <div>
-            <h1>This is header</h1>
-        </div>
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="primary">
+                <Container>
+                    <Navbar.Brand href="#home"><img style={{ width: '163px' }} src={logo} alt="" /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home" className='fw-bold'>Home</Nav.Link>
+                            <Nav.Link href="#inventory" className='fw-bold'>Inventory</Nav.Link>
+                            <Nav.Link href="#features" className='fw-bold'>Features</Nav.Link>
+                            <Nav.Link href="#demos" className='fw-bold'>Demos</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link className='fw-bold' eventKey={2} href="#singIn">
+                                Sign In
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
