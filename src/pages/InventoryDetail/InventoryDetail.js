83461a7abc582d78c9542a9cb501bf730fe78956
sumon092 <m.sumon@outlook.com>
2022-05-06 22:55:26 +0600

    delivered cart added

--- src/pages/InventoryDetail/InventoryDetail.js
+++ src/pages/InventoryDetail/InventoryDetail.js
@@ -5,11 +5,29 @@ import useInventoryDetail from '../hooks/useInventoryDetail';
 const InventoryDetail = () => {
     const { id } = useParams();
     const [inventory] = useInventoryDetail(id)
+    const { _id, img, name, price, supplier, quantity, description } = inventory;
     console.log(inventory);
     return (
-        <div>
-            <h2>You are going to {inventory._id}</h2>
-        </div>
+        <div className='container mt-5'>
+            <div className="card mb-3" style={{ width: "100%" }}>
+                <div className="row g-0">
+                    <div className="col-md-4">
+                        <img src={img} className="img-fluid rounded-start" alt="..." />
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{name}</h5>
+                            <p className="card-text">ID NO : <small className='text-muted'>{_id}</small></p>
+                            <p className="card-text"><small className='text-muted'>{description}</small></p>
+                            <p className="card-text">Price : ${price}</p>
+                            <p className="card-text">Supplier: {supplier}</p>
+                            <p className="card-text">Available Quantity : {quantity}</p>
+                            <button className='update-button'>DELEVERD</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div >
     );
 };
 
